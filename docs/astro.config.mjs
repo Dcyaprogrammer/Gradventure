15f8e77fd634d0a5f215934f7985b4e6a168e857
Dcyaprogrammer <dujustin6@gmail.com>
2026-04-03 15:22:16 +0800

    fix docs base

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dcyaprogrammer.github.io',
-  base: '/ApplicationWeb',
+  base: '/Gradventure',
   trailingSlash: 'always',
   build: {
     format: 'directory'
